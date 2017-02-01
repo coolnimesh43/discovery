1d37252bf6399f6f61252961b2ffc33b8b980839
coolnimesh43 <coolnimesh43@gmail.com>
2017-02-01 17:21:17 +0545

    Added files

---
+++ src/main/java/com/coolnimesh43/discovery/Application.java
@@ -0,0 +1,33 @@
+package com.coolnimesh43.discovery;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * Runner class for Euraka Discovery Server.
+ * 
+ * @author coolnimesh43
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) throws UnknownHostException {
+        Properties systemProperties = new Properties();
+        systemProperties.setProperty("spring.config.name", "registration-server");
+        System.setProperties(systemProperties);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, args);
+        Environment env = configurableApplicationContext.getEnvironment();
+        System.out.println("Starting Discovery Server at \n \n");
+        System.out.println("Localhost http://localhost:" + env.getProperty("server.port"));
+        System.out.println("External http://" + InetAddress.getLocalHost().getHostAddress() + ":" + env.getProperty("server.port"));
+    }
+}
