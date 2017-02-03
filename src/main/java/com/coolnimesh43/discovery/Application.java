59ba33eb239d6d2869c00377578909f71e4c6e68
coolnimesh43 <coolnimesh43@gmail.com>
2017-02-03 16:16:53 +0545

    changed log file name

--- src/main/java/com/coolnimesh43/discovery/Application.java
+++ src/main/java/com/coolnimesh43/discovery/Application.java
@@ -2,15 +2,12 @@ package com.coolnimesh43.discovery;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-
-import ch.qos.logback.core.util.EnvUtil;
 
 /**
  * Runner class for Euraka Discovery Server.
@@ -23,9 +20,6 @@ import ch.qos.logback.core.util.EnvUtil;
 public class Application {
 
     public static void main(String[] args) throws UnknownHostException {
-        System.out.println(EnvUtil.isWindows());
-        Properties systemProperties = new Properties();
-        systemProperties.setProperty("spring.config.name", "registration-server");
         System.setProperty("spring.config.name", "registration-server");
         ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, args);
         Environment env = configurableApplicationContext.getEnvironment();
